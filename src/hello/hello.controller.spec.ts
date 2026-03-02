95a416d1300e40a0314b79aa9127d79bc411746a
Rahul05 <trrahul05@gmail.com>
2026-03-02 23:57:25 +0530

    week_1

---
+++ src/hello/hello.controller.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HelloController } from './hello.controller';
+import { HelloService } from './hello.service';
+
+describe('HelloController', () => {
+    let controller: HelloController;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [HelloController],
+            providers: [HelloService],
+        }).compile();
+
+        controller = module.get<HelloController>(HelloController);
+    });
+
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+
+    it('GET /hello should return { message: "Hello World " }', () => {
+        expect(controller.getHello()).toEqual({ message: 'Hello World ' });
+    });
+});
+
+
